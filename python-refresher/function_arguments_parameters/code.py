5415f223af9947b7cefeecdfd5e7986c98e4dbee
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2022-01-26 11:28:14 -0300

    arguments and parameters

---
+++ python-refresher/function_arguments_parameters/code.py
@@ -0,0 +1,17 @@
+#      parameters  
+def add(x, y):
+    result = x + y
+    print(result)
+
+
+# arguments
+add(5, 3)
+
+print('---------------------')
+# --------------------------
+
+def say_hello(name, surname):
+    print(f'Hello, {name} {surname}')
+
+
+say_hello(surname='Ramos', name='Otavio')
---
+++ python-refresher/function_arguments_parameters/code2.py
@@ -0,0 +1,8 @@
+def divide(dividend, divisor):
+    if divisor != 0:
+        print(dividend / divisor)
+    else:
+        print('You fool!')
+
+
+divide(divisor=0, dividend=15)
