1c24ab90f8985f217eaeda2446933f2e09039f14
otavio.santana <otavio.santana@pd.tec.br>
2024-04-03 10:54:03 -0300

    Type hinting

---
+++ python-refresher/type_hinting/code.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def list_avg(sequence: List) -> float:
+    return sum(sequence) / len(sequence)
+
+list_avg([123])
+
+class Book:
+    @classmethod
+    def get_one(cls) -> "Book":
+        return cls()
+
+class Book:
+    def __init__(self, name: str, page_count: int):
+        self.name = name
+        self.page_count = page_count
+
+class BookShelf:
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self) -> str:
+        return f"BookShelf with {len(self.books)} books."
